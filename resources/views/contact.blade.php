cda8c765a11c1475ec2d56898435cc95b1b73473
unknown <ricok041098@gmail.com>
2020-06-06 21:18:30 -0400

    Página contato alterada utilizando flexbox

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,36 +2,32 @@
 
 @section('content')
 <div class="container-body-all">
-    <div class="img-background">
-        <img src="img/body-img.jpg" class="image-home">
+    <div class="boxes">
+        <div class="left-box-blured">
+            <div class="box-title">
+                <span>Contato</span>
+            </div>
+
+            <div class="box-message-contact">
+                <span>Telefone: 0800-SE-PRESERVE<br><br><br>E-mail: preserve@preserve.com</span>
+            </div>
+
+        </div>
+        <div class="right-box-blured">
+            <div class="box-title">
+                <span>Fale Conosco</span>
+            </div>
+
+            <div class="container">
+                <from>
+                    <input type="text" id="email" name="email" placeholder="Seu E-mail">
+
+                    <textarea id="subject" name="subject" placeholder="Sua Mensagem" style="height:100px"></textarea>
+
+                    <input type="submit" value="Enviar">
+                </form>
+            </div>
+        </div>
     </div>
-
-    <div class="right-box-blured">
-        <div class="box-title">
-            <span>Contato</span>
-        </div>
-
-        <div class="box-message-contact">
-            <span>Telefone: 0800-SE-PRESERVE<br><br><br>E-mail: preserve@preserve.com</span>
-        </div>
-
-    </div>
-    <div class="left-box-blured">
-        <div class="box-title">
-            <span>Fale Conosco</span>
-        </div>
-
-        
-        <div class="container">
-        <from>
-            <input type="text" id="email" name="email" placeholder="Seu E-mail">
-
-            <textarea id="subject" name="subject" placeholder="Sua Mensagem" style="height:100px"></textarea>
-
-            <input type="submit" value="Enviar">
-        </form>
-</div>
-
-
 </div>
 @endsection
