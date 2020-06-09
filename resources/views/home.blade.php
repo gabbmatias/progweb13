4378a01dd7f8e7822094c485fb487f4368e94580
ericcarickli <ericca.rickli>
2020-06-09 19:52:07 -0400

    Home alterada e tela Pla Planos preenchida

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,8 +9,43 @@
   </div>
 </div>
 <div class="instrucoes">
-  <div class="assine"></div>
-  <div class="assinaturas"></div>
-  <div class="produtos"></div>
+  <div class="assine">
+    <div class="assine-box">
+      <i class="fa fa-cart-plus"></i>
+      <p><strong>ESCOLHA O PLANO DE SUA PREFERÊNICA</strong></p>
+      <div class="description-icon">Adquira quantas camisinhas você desejar</div>
+    </div>
+  </div>
+
+  <div class="assinaturas">
+    <div class="assinaturas-left">
+      <i class="fa fa-credit-card" ></i>
+      <p><strong>CARTÃO DE CRÉDITO</strong></p>
+      <div class="description-icon">Aceitamos todas as bandeiras de cartão <br> para você realizar o seu pagamento</div>
+    </div>
+    <div class="assinaturas-right">
+      <i class="fa fa-file"></i>
+      <p><strong>EMISSÃO DE BOLETOS</strong></p>
+      <div class="description-icon">Emitimos boleto para você realizar <br> o seu pagamento</div>
+    </div>
+  
+  </div>
+  <div class="produtos">
+      <div class="produtos-left">
+          <i class="fa fa-calendar"></i>
+          <p><strong>TODO MÊS PARA VOCÊ</strong></p>
+          <div class="description-icon">Assine já e desfrute de todos o benefícios sem preocupação</div>
+      </div>
+      <div class="produtos-right">
+        <i class="fa fa-home"></i>
+        <p><strong>RECEBA SEU PRODUTO EM CASA</strong></p>
+        <div class="description-icon">Receba seu produto com segurança no conforto de sua residência</div>
+      </div>
+      <div class="produtos-center">
+          <i class="fa fa-truck"></i>
+          <p><strong>FRETE GRÁTIS</strong></p>
+          <div class="description-icon">Entregamos para qualquer região do Brasil</div>
+      </div>
+  </div>
 </div>
 @endsection
