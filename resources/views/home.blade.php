cd02b0c4c06fb4f4005e9fc010db022ebd45ea9f
ericcarickli <ericca.rickli>
2020-06-09 20:27:19 -0400

    barra com os ícones alterada

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,23 +10,28 @@
 </div>
 <div class="instrucoes">
   <div class="assine">
-    <div class="assine-box">
+    <div class="assine-left">
       <i class="fa fa-cart-plus"></i>
       <p><strong>ESCOLHA O PLANO DE SUA PREFERÊNICA</strong></p>
-      <div class="description-icon">Adquira quantas camisinhas você desejar</div>
+      <div class="description-icon">Adquira quantas camisinhas <br> você desejar</div>
     </div>
+    <div class="assine-right">
+          <i class="fa fa-truck"></i>
+          <p><strong>FRETE GRÁTIS</strong></p>
+          <div class="description-icon">Entregamos para qualquer <br> região do Brasil</div>
+      </div>
   </div>
 
   <div class="assinaturas">
     <div class="assinaturas-left">
       <i class="fa fa-credit-card" ></i>
       <p><strong>CARTÃO DE CRÉDITO</strong></p>
-      <div class="description-icon">Aceitamos todas as bandeiras de cartão <br> para você realizar o seu pagamento</div>
+      <div class="description-icon">Aceitamos todas as bandeiras de <br> cartão para você realizar o seu <br> pagamento</div>
     </div>
     <div class="assinaturas-right">
       <i class="fa fa-file"></i>
       <p><strong>EMISSÃO DE BOLETOS</strong></p>
-      <div class="description-icon">Emitimos boleto para você realizar <br> o seu pagamento</div>
+      <div class="description-icon">Emitimos boleto para você <br> realizar o seu pagamento</div>
     </div>
   
   </div>
@@ -34,18 +39,18 @@
       <div class="produtos-left">
           <i class="fa fa-calendar"></i>
           <p><strong>TODO MÊS PARA VOCÊ</strong></p>
-          <div class="description-icon">Assine já e desfrute de todos o benefícios sem preocupação</div>
+          <div class="description-icon">Assine já e desfrute de todos <br> os benefícios sem preocupação</div>
       </div>
       <div class="produtos-right">
         <i class="fa fa-home"></i>
         <p><strong>RECEBA SEU PRODUTO EM CASA</strong></p>
-        <div class="description-icon">Receba seu produto com segurança no conforto de sua residência</div>
+        <div class="description-icon">Receba seu produto com segurança <br> no conforto de sua residência</div>
       </div>
-      <div class="produtos-center">
+      <!-- <div class="produtos-center">
           <i class="fa fa-truck"></i>
           <p><strong>FRETE GRÁTIS</strong></p>
           <div class="description-icon">Entregamos para qualquer região do Brasil</div>
-      </div>
+      </div> -->
   </div>
 </div>
 @endsection
