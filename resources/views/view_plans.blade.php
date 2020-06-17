0b8d19154e7837e8206cce7b9125cb2c7b853336
unknown <ricok041098@gmail.com>
2020-06-17 16:19:21 -0400

    arrumado css da página view plans

--- resources/views/view_plans.blade.php
+++ resources/views/view_plans.blade.php
@@ -3,35 +3,36 @@
 @section('content')
 <div class="viewPlansBody">
     <a class="viewPlansTitle">Planos</a>
+    <div class="viewPlansBoxes">
+        @foreach ($plans as $plan)
 
-    @foreach ($plans as $plan)
+        <div class="boxViewPlano">
+            <div class="box-plano-titulo">
+            <a>{{ $plan->plan_name}}</a>
+            </div>
+            <div class="box-plano-text">
+                <a> {{ $plan->description}}<br><strong>Apenas R$ {{ $plan->price }}!</strong></a>
+            </div>
+            <div class="actions">
 
-        <div class="viewPlansBoxes">
-            <div class="boxViewPlano">
-                <div class="box-plano-titulo">
-                <a>{{ $plan->plan_name}}</a>
-                </div>
-                <div class="box-plano-text">
-                    <a> {{ $plan->description}}
-                        <br><strong>Apenas R$ {{ $plan->price }}!</strong></a>
-                </div>
-                <div class="actions">
-
-                    <form id="update-form" action="{{ route('plan.edit') }}" method="POST">
-                        @csrf
-                        <input hidden name="plan_id" value="{{ $plan->plan_id }}">
-                        <input type="submit" name="submit" value="Editar">
-                    </form>
+                <form id="update-form" action="{{ route('plan.edit') }}" method="POST">
+                    @csrf
+                    <input hidden name="plan_id" value="{{ $plan->plan_id }}">
+                    <input type="submit" class="action" name="submit" value="Editar">
+                </form>
                     
-                    <form id="update-form" action="{{ route('plan.destroy') }}" method="POST">
-                        @csrf
-                        <input hidden name="plan_id" value="{{ $plan->plan_id }}">
-                        <input type="submit" name="submit" value="Remover">
-                    </form>
-                </div>
+                <form id="update-form" action="{{ route('plan.destroy') }}" method="POST">
+                    @csrf
+                    <input hidden name="plan_id" value="{{ $plan->plan_id }}">
+                    <input type="submit" class="action" name="submit" value="Remover">
+                </form>
             </div>
         </div>
-    @endforeach
-
+        @endforeach
+    </div>
+    <div class="addPlan">
+        <input type="submit" value="+">
+        <a href="{{ route('plan.create') }}">Adicionar Plano</a>
+    </div>
 </div>
 @endsection
