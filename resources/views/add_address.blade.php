36ff23397f24a7a1d0b1339a1a243c2fa21fb9ee
Igor Braga <igorbgalvan@github.com>
2020-06-25 04:36:47 -0400

    Logger user resposive updated, required inputs (card, plan and address)

--- resources/views/add_address.blade.php
+++ resources/views/add_address.blade.php
@@ -6,15 +6,15 @@
 
         <form class="addressForm" method="POST" action="{{ route('address.store') }}">
             @csrf
-            <input type="text" name="client_id" id="client_id" value="{{ Auth::user()->id }}" hidden>
-            <input type="text" name="street" id="street" placeholder="Rua">
-            <input type="text" name="street_number" id="street_number" placeholder="Número">
-            <input type="text" name="complement" id="complement" placeholder="Complemento">
-            <input type="text" name="neighborhood" id="neighborhood" placeholder="Bairro">
-            <input type="text" name="cep" id="cep" placeholder="CEP">
-            <input type="text" name="city" id="city" placeholder="Cidade">
-            <input type="text" name="state" id="state" placeholder="Estado">
-            <input type="text" name="country" id="country" placeholder="País">
+            <input required type="text" name="client_id" id="client_id" value="{{ Auth::user()->id }}" hidden>
+            <input required type="text" name="street" id="street" placeholder="Rua">
+            <input required type="text" name="street_number" id="street_number" placeholder="Número">
+            <input required type="text" name="complement" id="complement" placeholder="Complemento">
+            <input required type="text" name="neighborhood" id="neighborhood" placeholder="Bairro">
+            <input required type="text" name="cep" id="cep" placeholder="CEP">
+            <input required type="text" name="city" id="city" placeholder="Cidade">
+            <input required type="text" name="state" id="state" placeholder="Estado">
+            <input required type="text" name="country" id="country" placeholder="País">
 
             <input type="submit" value="Cadastrar Endereço">
         </form>
--- resources/views/registerplans.blade.php
+++ resources/views/registerplans.blade.php
@@ -6,9 +6,9 @@
 
 <form class="registerPlansForm" method="POST" action="{{ route('plan.store') }}" >
     @csrf
-        <input type="text" name="plan_name" id="plan_name" placeholder="Nome Plano">
-        <input type="text" name="price" id="price" placeholder="Preço">
-        <textarea type="text" name="description" id="description" placeholder="Descrição" style="height:100px"></textarea>
+        <input required type="text" name="plan_name" id="plan_name" placeholder="Nome Plano">
+        <input required type="text" name="price" id="price" placeholder="Preço">
+        <textarea required type="text" name="description" id="description" placeholder="Descrição" style="height:100px"></textarea>
         <input type="submit" value="Cadastrar Plano">
     <form>      
 
