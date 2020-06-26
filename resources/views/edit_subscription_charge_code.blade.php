f12fab0d541b1024479b33a91bf165d9e8402350
ericcarickli <ericca.rickli>
2020-06-26 18:17:51 -0400

    Tela editar pagamento da assinatura - código do boleto

---
+++ resources/views/edit_subscription_charge_code.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="compraFinalizdaBody">
+    
+    <p>Assinatura Realizada!</p>
+    <div class="confirmationTitle">
+        
+        <h5>Código de barras:</h5>
+            <h6>Código do Boleto</h6>
+            <div class="downloadBoleto">
+                <a href="#">Download Boleto</a>
+            </div>
+       
+    </div>
+    <a href="#" class="retornarHome">Me leve à pagina príncipal</a>
+</div>
+@endsection
