8153108f0d04d72b4b300a26fe909c003e62691b
jardimguilherme <guilhermejardim@live.com>
2020-06-25 20:06:08 -0400

    completed transaction new page

--- resources/views/completed_transaction.blade.php
+++ resources/views/completed_transaction.blade.php
@@ -5,14 +5,14 @@
     <div class="imagemFluxo">
         <img src="/img/routeBar-confirmation.png">
     </div>
+    <p>Assinatura Realizada!</p>
     <div class="confirmationTitle">
         @if ($charge_code != null)
-        <h5>Codigo de barras</h5>
+        <h5>Codigo de barras:</h5>
             <h6>{{ $charge_code }}</h6>
 
             <a href="{{ route("charge.create") }}">Download Boleto</a>
         @endif
-        <p>Pedido Confirmado!</p>
     </div>
 </div>
 @endsection
