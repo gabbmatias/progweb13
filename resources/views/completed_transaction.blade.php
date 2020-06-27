1f51a25d2988c0b3b819336e7168619bab0fed5d
jardimguilherme <guilhermejardim@live.com>
2020-06-27 17:35:02 -0400

    new linkbar

--- resources/views/completed_transaction.blade.php
+++ resources/views/completed_transaction.blade.php
@@ -6,13 +6,13 @@
         <img src="/img/routeBar-confirmation.png">
     </div>
     <div class="confirmationTitle">
-    <p>Assinatura Realizada!</p><br>
+        <p>Assinatura Realizada!</p><br>
         @if ($charge_code != null)
-        <h5>Código de barras:</h5>
-            <h6>{{ $charge_code }}</h6>
-            <div class="downloadBoleto">
-                <a href="{{ route('charge.create') }}">Download Boleto</a>
-            </div>
+        <h1>Código de barras:</h1>
+        <h2>{{ $charge_code }}</h2>
+        <div class="downloadBoleto">
+            <a href="{{ route('charge.create') }}">Download Boleto</a>
+        </div>
         @endif
     </div>
     <a href="{{ route('home') }}" class="retornarHome">Me leve à pagina príncipal</a>
