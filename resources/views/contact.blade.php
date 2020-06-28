20fa89c477408aa4dd3aafcb161dd8020db3a9b0
Igor Braga <igorbgalvan@github.com>
2020-06-27 22:09:05 -0400

    minor fixes

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -12,8 +12,8 @@
 
             <form method="POST" onsubmit="alert('Sua mensagem foi enviada com sucesso!')" action="{{ route('contact.store') }}">
                 @csrf
-                    <input type="text" id="email" name="email" placeholder="Seu E-mail" require>
-                    <textarea id="subject" name="message" placeholder="Sua Mensagem" style="height:100px" require></textarea>
+                    <input type="text" id="email" name="email" placeholder="Seu E-mail" required>
+                    <textarea id="subject" name="message" placeholder="Sua Mensagem" style="height:100px" required></textarea>
                     <input type="submit" class="enviar" value="Enviar">
                 </form>
             </div>
