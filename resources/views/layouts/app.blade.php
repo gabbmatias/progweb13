c02165eda2d9df00eea71e5e7f9916326a9759d3
jardimguilherme <guilhermejardim@live.com>
2020-06-28 00:07:35 -0400

    project fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,9 +36,9 @@
     <link href="{{ asset('css/home-styles.css') }}" rel="stylesheet">
     <link href="{{ asset('css/view-address-styles.css') }}" rel="stylesheet">
     <link href="{{ asset('css/view-contact-styles.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/view-logs-styles.css') }}" rel="stylesheet">
     <link href="{{ asset('css/view-plans-styles.css') }}" rel="stylesheet">
     <link href="{{ asset('css/view-subscriptions-styles.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/logs-history-styles.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
